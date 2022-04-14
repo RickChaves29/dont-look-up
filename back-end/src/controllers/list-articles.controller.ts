8ad215d5161f54857ea52cf1ad201573a3c97ef0
Henrique chaves <canaldomonza@gmail.com>
2022-04-14 10:29:28 -0300

    added controller to list all articles

---
+++ back-end/src/controllers/list-articles.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { ListArticlesService } from 'src/services/list-articles.service';
+
+@Controller('articles')
+export class ListArticlesController {
+  constructor(private service: ListArticlesService) {}
+  @Get()
+  async list(@Query('page') page?: string, @Query('limit') limit?: string) {
+    return await this.service.list(+page, +limit);
+  }
+}
