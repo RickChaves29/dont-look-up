185c4f892ce0cccbe69c48d9331d48d14550de9e
Henrique chaves <canaldomonza@gmail.com>
2022-04-14 10:28:24 -0300

    added controller to create articles

---
+++ back-end/src/controllers/create-article.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateArticleDTO } from 'src/dto/create-article.dto';
+import { CreateArticleService } from 'src/services/create-article.service';
+
+@Controller('articles')
+export class CreateAriticleController {
+  constructor(private service: CreateArticleService) {}
+  @Post()
+  async create(@Body() data: CreateArticleDTO) {
+    return await this.service.create(data);
+  }
+}
