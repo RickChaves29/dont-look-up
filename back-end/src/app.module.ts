2cc063f7a356e9bb9c9c34a8554748de61d29fb0
Henrique chaves <canaldomonza@gmail.com>
2022-04-15 23:43:35 -0300

    feat: update modules on app module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CreateArticleModule } from './modules/create-article.module';
 import { DeleteArticleModule } from './modules/delete-article.module';
 import { ListArticlesModule } from './modules/list-articles.module';
 import { ListOneArticleModule } from './modules/list-one-aritcle.module';
+import { NewArticleJobModule } from './modules/new-article.job.module';
 import { UpdateArticleModule } from './modules/update-article.module';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
+    NewArticleJobModule,
     CreateArticleModule,
     ListArticlesModule,
     ListOneArticleModule,
